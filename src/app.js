c7e9ca6380494a5389ff75b911e940b5d889f422
Vikash <empirevikash@gmail.com>
2025-01-12 22:52:29 +0530

    Exploring GET, POST, DELET

--- src/app.js
+++ src/app.js
@@ -4,16 +4,45 @@ const express = require('express');
 
 const app = express(); // this is an insatnce of your exprssjs server
 
-app.use("/", (req, res)=>{
-    res.send("Hello from the dashboard"); // This functions known is request handler
-});
+// app.use("/", (req, res)=>{
+//     res.send("Hello from the dashboard"); // This functions known is request handler
+// It will take first always this router it will override all the routeer
+// so we have to keep this at the bottom of all the router
+// });
 
+
+// ## ORDER IS VERY MUCH MATTER
+// This will only handle GET call to /user 
+app.get("/user", (req, res)=>{
+    res.send({firstName:"Akshya", lastame:"Saini"});
+
+})
+
+app.post("/user", (req, res)=>{
+    // Saving data to DB
+    // console.log("Save Data successfuly");
+    res.send("Data save successfully")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted successfully !!")
+})
+
+// this will match all the HTTP methodAPI calls to /test
 app.use("/test", (req, res)=>{
-    res.send("Hello from the test router"); // This functions known is request handler
+    res.send("Hello from the test router"); // This functions known is request handler //
 });
 
 app.use("/hello", (req, res)=>{
     res.send("Hello from the Hello router"); // This functions known is request handler
+});
+
+
+app.use("/", (req, res)=>{
+    res.send("Hello from the dashboard"); // This functions known is request handler
+// It will take first always this router it will override all the routeer
+// so we have to keep this at the bottom of all the router // now it will works fine
+// ## Order is very imp.
 });
 
 app.listen(3000, ()=>{
